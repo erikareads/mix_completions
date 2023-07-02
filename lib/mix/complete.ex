939e5369ab3bfec4ad175f6119b0ac111d21112d
Erika Rowland <accounts@erikarowland.com>
2023-07-02 14:09:15 -0700

    feat: Support extra help text in bash generation

--- lib/mix/complete.ex
+++ lib/mix/complete.ex
@@ -9,22 +9,28 @@ defmodule Mix.Tasks.Complete do
     aliases = load_aliases()
 
     docs = build_doc_list(modules, aliases)
-    text = docs |> Enum.sort() |> Enum.map(fn {task, shortdoc} -> "#{task}\t#{shortdoc}" end) |> Enum.join("\n")
+
+    text =
+      docs
+      |> Enum.sort()
+      |> Enum.map(fn {task, shortdoc} -> "#{task}\t#{shortdoc}" end)
+      |> Enum.join("\n")
+
     # text = elem(System.shell("mix help --names"), 0)
-    cache_dir = case System.get_env("XDG_CACHE_HOME") do
-      nil -> Path.join([System.get_env("HOME"), ".cache"])
-      "" -> Path.join([System.get_env("HOME"), ".cache"])
-      xdg_cache_home -> xdg_cache_home
-    end
+    cache_dir =
+      case System.get_env("XDG_CACHE_HOME") do
+        nil -> Path.join([System.get_env("HOME"), ".cache"])
+        "" -> Path.join([System.get_env("HOME"), ".cache"])
+        xdg_cache_home -> xdg_cache_home
+      end
 
     if File.exists?("mix.exs") do
       File.write(".mix_complete.cache", text)
       IO.puts(:stderr, "cache updated in project directory")
     else
-      File.write(Path.join([cache_dir, ".mix_complete.cache"]), text) 
+      File.write(Path.join([cache_dir, ".mix_complete.cache"]), text)
       IO.puts(:stderr, "cache updated in #{cache_dir}")
     end
-
   end
 
   defp loadpaths! do
@@ -68,6 +74,4 @@ defmodule Mix.Tasks.Complete do
       [{task, doc} | docs]
     end)
   end
-
-  end
-
+end
